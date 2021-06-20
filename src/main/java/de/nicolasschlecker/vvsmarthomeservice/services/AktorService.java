5912bcb7de80d248556026da4c97a661724144c7
Nicolas Schlecker <nicolasschlecker@gmail.com>
2021-06-20 19:27:44 +0200

    Created AktorService

---
+++ src/main/java/de/nicolasschlecker/vvsmarthomeservice/services/AktorService.java
@@ -0,0 +1,73 @@
+package de.nicolasschlecker.vvsmarthomeservice.services;
+
+import de.nicolasschlecker.vvsmarthomeservice.domain.aktor.Aktor;
+import de.nicolasschlecker.vvsmarthomeservice.domain.aktor.PersistentAktor;
+import de.nicolasschlecker.vvsmarthomeservice.repositories.AktorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.sql.Timestamp;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+@Service
+public class AktorService {
+    private final AktorRepository mRepository;
+
+    @Autowired
+    public AktorService(AktorRepository mRepository) {
+        this.mRepository = mRepository;
+    }
+
+    public Optional<Aktor> create(Aktor entity) {
+        if (mRepository.existsById(entity.getId())) {
+            return Optional.empty();
+        }
+
+        final var persistentEntity = persistentEntityFromEntity(entity);
+        return Optional.of(entityFromPersistentEntity(mRepository.save(persistentEntity)));
+    }
+
+    public List<Aktor> findAll() {
+        return StreamSupport
+                .stream(mRepository.findAll().spliterator(), false)
+                .filter(p -> p.getDeletedAt() != null)
+                .map(this::entityFromPersistentEntity)
+                .collect(Collectors.toList());
+    }
+
+    public Optional<Aktor> find(Long sensorId) {
+        final var optionalPersistentEntity = mRepository.findById(sensorId);
+
+        return optionalPersistentEntity.map(e -> {
+            if (e.getDeletedAt() == null) {
+                return entityFromPersistentEntity(e);
+            }
+            return null;
+        });
+    }
+
+    protected Aktor entityFromPersistentEntity(PersistentAktor persistentAktor) {
+        final var aktor = new Aktor();
+        aktor.setId(persistentAktor.getId());
+        aktor.setName(persistentAktor.getName());
+        aktor.setLocation(persistentAktor.getLocation());
+        aktor.setServiceUrl(persistentAktor.getServiceUrl());
+        aktor.setCreatedAt(persistentAktor.getCreatedAt().toLocalDateTime());
+        aktor.setUpdatedAt(persistentAktor.getUpdatedAt().toLocalDateTime());
+        return aktor;
+    }
+
+    protected PersistentAktor persistentEntityFromEntity(Aktor aktor) {
+        final var persistentAktor = new PersistentAktor();
+        persistentAktor.setId(aktor.getId());
+        persistentAktor.setName(aktor.getName());
+        persistentAktor.setLocation(aktor.getLocation());
+        persistentAktor.setServiceUrl(aktor.getServiceUrl());
+        persistentAktor.setCreatedAt(Timestamp.valueOf(aktor.getCreatedAt()));
+        persistentAktor.setUpdatedAt(Timestamp.valueOf(aktor.getUpdatedAt()));
+        return persistentAktor;
+    }
+}
