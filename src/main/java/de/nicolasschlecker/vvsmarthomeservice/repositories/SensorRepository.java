91b4950b9818f90e72ba80ba5cd4978b97cc6160
Nicolas Schlecker <nicolasschlecker@gmail.com>
2021-06-20 19:27:19 +0200

    Created SensorRepository

--- src/main/java/de/nicolasschlecker/vvsmarthomeservice/repositories/SensorRepository.java
+++ src/main/java/de/nicolasschlecker/vvsmarthomeservice/repositories/SensorRepository.java
@@ -1,9 +1,9 @@
 package de.nicolasschlecker.vvsmarthomeservice.repositories;
 
-import de.nicolasschlecker.vvsmarthomeservice.domain.Sensor;
+import de.nicolasschlecker.vvsmarthomeservice.domain.sensor.PersistentSensor;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SensorRepository extends CrudRepository<Sensor, Long> {
+public interface SensorRepository extends CrudRepository<PersistentSensor, Long> {
 }
