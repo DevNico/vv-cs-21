ca808b415f491aafa6b84b5f39bb865bc8f60ffe
Nicolas Schlecker <nicolasschlecker@gmail.com>
2021-06-21 10:30:08 +0200

    Fixed RuleMapper

--- src/main/java/de/nicolasschlecker/vvsmarthomeservice/common/RuleMapper.java
+++ src/main/java/de/nicolasschlecker/vvsmarthomeservice/common/RuleMapper.java
@@ -3,10 +3,13 @@ package de.nicolasschlecker.vvsmarthomeservice.common;
 import de.nicolasschlecker.vvsmarthomeservice.domain.rule.PersistentRule;
 import de.nicolasschlecker.vvsmarthomeservice.domain.rule.Rule;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.springframework.stereotype.Component;
 
 @Component
 @Mapper(componentModel = "spring", uses = TimestampLocalDateTimeMapper.class)
 public interface RuleMapper {
+    @Mapping(target = "sensorId", source = "sensor.id")
+    @Mapping(target = "aktorId", source = "aktor.id")
     Rule fromPersistent(PersistentRule persistentRule);
 }
